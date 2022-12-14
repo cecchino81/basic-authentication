7a42c8ecb4b5d440c35f7b9444038cd64b74a9f2
Rosario Mezzatesta <ra.mezzatesta@almaviva.it>
2022-12-14 09:40:36 +0100

    Primo caricamento

---
+++ src/main/java/com/lentux/basicauth/SecurityWebApplication.java
@@ -0,0 +1,13 @@
+package com.lentux.basicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecurityWebApplication.class, args);
+	}
+
+}
